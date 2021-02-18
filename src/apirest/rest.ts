d2ed9724382ec6ce99ec1069b7bfb6022cd70d95
JDR <jrosenblatt@fi.uba.ar>
2021-02-18 15:59:10 -0300

    More stuff for the bot

--- src/apirest/rest.ts
+++ src/apirest/rest.ts
@@ -11,27 +11,23 @@ import { Bot } from '../server/bot';
 // const bot: Bot = new Bot();
 
 router.get('/', (req: any, res: any) => {
-    console.log(req);
     res.send("Hello World!");
 });
 
 router.get('/data/', (req: any, res: any) => {
-    console.log(req);
     res.send("Hello Data!");
 });
 
 router.post('/', (req: any, res: any) => {
-    console.log(req);
     res.send("Hello Post!");
 });
 
 router.delete('/', (req: any, res: any) => {
-    console.log(req);
     res.send("Hello Delete!");
 });
 
 app.use(router);
 
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log("Node server running on http://localhost:3000");
 });
